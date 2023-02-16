494b5db7c4a4cbe68f1e079905f07961f2fcd291
TEEJAYGLITZ <tajluk2010@gmail.com>
2023-02-16 15:07:37 -0800

    betty

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints a given phrase
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
