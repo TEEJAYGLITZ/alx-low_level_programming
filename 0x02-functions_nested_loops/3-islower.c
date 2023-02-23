c4f350e70fd672a2e6324f853b31d337913f8f7a
TEEJAYGLITZ <tajluk2010@gmail.com>
2023-02-22 18:11:07 -0800

    c3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -14,11 +14,11 @@
 
 int _islower(int c)
 {
- if (c >= 'a' && c <= 'z')
- {
- return (1); }
- else
- {
- return (0);
- }
+	if (c >= 'a' && c <= 'z')
+	{
+	return (1);                                                       }
+	else
+	{
+	return (0);
+	}
 }
