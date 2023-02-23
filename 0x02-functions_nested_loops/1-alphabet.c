deb2d3565a54ae946f9b0588083aa949353e7a19
TEEJAYGLITZ <tajluk2010@gmail.com>
2023-02-22 18:03:48 -0800

    C1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,21 +2,23 @@
 
 
 /**
- * print_alphabet-entry point
- * Description; function that prints the alphabet, in lowercase,
+ * print_alphabet - entry point
+ * Description: function that prints the alphabet, in lowercase,
  * followed by a new line
  *
  * Return: Always 0
  *
  */
+
 void print_alphabet(void)
 {
-	char letter ='a';
+	char letter = 'a';
 
-	while (letter <='z')
+	while (letter <= 'z')
 	{
 		_putchar(letter);
 		letter++;
 	}
-	_putchar('\n')
+	_putchar('\n');
+
 }
